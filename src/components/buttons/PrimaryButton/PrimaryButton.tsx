8e95d90af115930005b82d59230a21a334cbd15c
airfortech <airm@xxx.yy>
2022-07-21 18:17:00 +0200

    fixed eslint problems with proptypes

--- src/components/buttons/PrimaryButton/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import classes from './PrimaryButton.module.css';
 
-interface Props {
+export interface Props {
   children: string;
   type?: 'button' | 'submit';
 }
 
 export const PrimaryButton = ({ children, type }: Props) => {
-  return (
-    <button type={type} className={classes.PrimaryButton}>
+  return type === 'button' ? (
+    <button type="button" className={classes.PrimaryButton}>
+      {children}
+    </button>
+  ) : (
+    <button type="submit" className={classes.PrimaryButton}>
       {children}
     </button>
   );
