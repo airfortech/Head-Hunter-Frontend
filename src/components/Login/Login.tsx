e0082bc2ee50f531870254ba5c36eb7bda2df45a
airfortech <airm@xxx.yy>
2022-07-22 12:00:36 +0200

    added Input and finished LoginView

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import style from './Login.module.css';
-import logo from '../../assets/images/logo.png';
 import { PrimaryButton } from '../buttons/PrimaryButton/PrimaryButton';
+import logo from '../../assets/images/logo.png';
+import classes from './Login.module.css';
+import { Input } from './Input/Input';
 
 export const Login = () => {
   return (
-    <main className={style.login}>
-      <img src={logo} alt="MegaK Logo" className={style.logo} />
-      <form>
-        <p>Email input</p>
-        <p>Password input</p>
-        <PrimaryButton type="submit" color="quaternary" size="large">
-          Zaloguj się
-        </PrimaryButton>
+    <main className={classes.Login}>
+      <img src={logo} alt="MegaK Logo" className={classes.logo} />
+      <form className={classes.form}>
+        <Input type="email" placeholder="E-mail" />
+        <Input type="password" placeholder="Password" />
+        <div className={classes.buttons}>
+          <p className={classes.link}>Zapomniałeś hasła?</p>
+          <PrimaryButton type="submit" color="primary" size="large">
+            Zaloguj się
+          </PrimaryButton>
+        </div>
       </form>
     </main>
   );
--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Login } from '../../components/Login/Login';
-import style from './LoginView.module.css';
+import classes from './LoginView.module.css';
 
 export const LoginView = () => {
   return (
-    <div className={style.loginView}>
+    <div className={classes.LoginView}>
       <Login />
     </div>
   );
