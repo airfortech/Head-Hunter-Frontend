7ec94f2a2e9cc6f2963bfbf6d7dcf8ae1615268b
airfortech <airm@xxx.yy>
2022-07-22 10:24:45 +0200

    changed PrimaryButton

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -11,6 +11,15 @@ export const Login = () => {
         <p>Email input</p>
         <p>Password input</p>
         <PrimaryButton>Zaloguj się</PrimaryButton>
+        <PrimaryButton color="secondary" size="small">
+          Zaloguj się
+        </PrimaryButton>
+        <PrimaryButton color="tertiary" size="small">
+          Zaloguj się
+        </PrimaryButton>
+        <PrimaryButton type="submit" color="quaternary" size="large">
+          Zaloguj się 2
+        </PrimaryButton>
       </form>
     </main>
   );
--- src/components/buttons/PrimaryButton/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import classes from './PrimaryButton.module.css';
 
-export interface Props {
+interface Props {
   children: string;
   type?: 'button' | 'submit';
+  size?: 'small' | 'normal' | 'large';
+  color?: 'primary' | 'secondary' | 'tertiary' | 'quaternary';
 }
 
-export const PrimaryButton = ({ children, type }: Props) => {
-  return type === 'button' ? (
-    <button type="button" className={classes.PrimaryButton}>
-      {children}
-    </button>
+export const PrimaryButton = ({ children, type, color, size }: Props) => {
+  const sizeClass = size || 'normal';
+  const colorClass = color || 'primary';
+
+  return type === 'submit' ? (
+    <input
+      type="submit"
+      value={children}
+      className={`${classes.PrimaryButton} ${classes[sizeClass]} ${classes[colorClass]}`}
+    />
   ) : (
-    <button type="submit" className={classes.PrimaryButton}>
+    <button
+      type="button"
+      className={`${classes.PrimaryButton} ${classes[sizeClass]} ${classes[colorClass]}`}
+    >
       {children}
     </button>
   );
