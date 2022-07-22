66b193e50feddd7e9b54a895f9c0f81ff7f161d8
airfortech <airm@xxx.yy>
2022-07-22 12:53:30 +0200

    added HRView

---
+++ src/views/HRView/HRView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './HRView.module.css';
+
+export const HRView = () => {
+  return (
+    <div className={classes.HRView}>
+      <h1>HR View</h1>
+    </div>
+  );
+};
