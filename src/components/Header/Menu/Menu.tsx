9384a3894fae66edbfcbb647ff253096d5685a18
airfortech <airm@xxx.yy>
2022-07-26 00:22:54 +0200

    fixed any in TS

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -6,43 +6,54 @@ import classes from './Menu.module.css';
 export const Menu = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const actionsRef = useRef<HTMLDivElement>(null);
-  const actionsItemsRef = useRef<HTMLDivElement>(null);
   const iconRef = useRef<HTMLElement>(null);
-  const tweenRef = useRef<any>(null);
+  const tweenRef = useRef<GSAPTimeline>();
 
   const handleClick = () => {
     setMenuOpen((prevState) => !prevState);
   };
 
   useEffect(() => {
+    const actionsItems = actionsRef.current?.children;
+
+    if (!actionsItems?.length) return;
+
     tweenRef.current = gsap
       .timeline()
       .to(iconRef.current, {
         ease: 'power3',
         rotation: 180,
-        duration: 0.2,
+        duration: 0.3,
       })
-      .to(actionsRef.current, {
-        ease: 'power3',
-        height: 'auto',
-        duration: 0.2,
-      })
-      .to(actionsItemsRef.current, {
-        ease: 'power3',
-        opacity: 1,
-        duration: 0.2,
-      });
-
+      .to(
+        actionsRef.current,
+        {
+          ease: 'power3',
+          height: 'auto',
+          duration: 0.2,
+        },
+        '<-=0.05'
+      )
+      .to(
+        actionsItems,
+        {
+          ease: 'power3',
+          opacity: 1,
+          duration: 0.3,
+          stagger: 0.1,
+        },
+        '<+=0.2'
+      );
     return () => {
-      tweenRef.current.kill();
+      tweenRef.current?.kill();
     };
   }, []);
 
   useEffect(() => {
     if (isMenuOpen) {
-      tweenRef.current.play();
+      tweenRef.current?.play();
     } else {
-      tweenRef.current.reverse();
+      tweenRef.current?.reverse();
     }
   }, [isMenuOpen]);
 
@@ -54,10 +65,8 @@ export const Menu = () => {
         <i className="bx bx-caret-up" ref={iconRef}></i>
       </button>
       <div className={classes.actions} ref={actionsRef}>
-        <div ref={actionsItemsRef} className={classes.actionsItems}>
-          <p>Konto</p>
-          <p>Wyloguj</p>
-        </div>
+        <p>Konto</p>
+        <p>Wyloguj</p>
       </div>
     </menu>
   );
