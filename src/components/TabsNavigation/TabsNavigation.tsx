46de9743bc0e2f96a3cc1f5644a7c18ad75416bb
airfortech <airm@xxx.yy>
2022-07-28 10:03:47 +0200

    added StudentsView and TabsNavigation

---
+++ src/components/TabsNavigation/TabsNavigation.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './TabsNavigation.module.css';
+
+export const TabsNavigation = () => {
+  return <div className={classes.TabsNavigation}>TabsNavigation</div>;
+};
---
+++ src/views/StudentsListView/StudentsListView.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './StudentsListView.module.css';
+
+export const StudentsListView = () => {
+  return <div className={classes.StudentsListView}>StudentsListView</div>;
+};
