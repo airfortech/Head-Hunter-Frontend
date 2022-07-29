d9e6da341519129a0272f6bdce01340740e80c1c
airfortech <airm@xxx.yy>
2022-07-29 13:12:55 +0200

    added custom-scrollbars-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Scrollbars from 'react-custom-scrollbars-2';
 import { LoginView } from './views/LoginView/LoginView';
 import { HRView } from './views/HRView/HRView';
 import { CVView } from './views/CVView/CVView';
+import classes from './App.module.css';
 
 export const App = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LoginView />} />
-        <Route path="/hr" element={<HRView />} />
-        <Route path="/cv" element={<CVView />} />
-      </Routes>
+    <div className={classes.App}>
+      <Scrollbars autoHide style={{ width: '100%', height: '100vh' }}>
+        <Routes>
+          <Route path="/" element={<LoginView />} />
+          <Route path="/hr" element={<HRView />} />
+          <Route path="/cv" element={<CVView />} />
+        </Routes>
+      </Scrollbars>
     </div>
   );
 };
