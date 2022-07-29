cb15a32e7a29972d9930ec28574337fc9ca0504b
airfortech <airm@xxx.yy>
2022-07-29 23:51:26 +0200

    fixed scrollbar initial behaviour

--- src/components/Scrollbar/Scrollbar.tsx
+++ src/components/Scrollbar/Scrollbar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Scrollbars from 'react-custom-scrollbars-2';
 import classes from './Scrollbar.module.css';
 
@@ -7,24 +7,26 @@ interface Props {
 }
 
 export const Scrollbar = ({ children }: Props) => {
-  const [isMouseMoving, setIsMouseMoving] = useState(false);
+  const [hideScrollbar, setHideScrollbar] = useState(false);
   const timeout: any = useRef(null);
 
   const handleMouseActivity = () => {
-    setIsMouseMoving(false);
+    setHideScrollbar(false);
 
-    (() => {
-      clearTimeout(timeout.current);
-      timeout.current = setTimeout(() => setIsMouseMoving(true), 2500);
-    })();
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => setHideScrollbar(true), 2500);
   };
+
+  useEffect(() => {
+    timeout.current = setTimeout(() => setHideScrollbar(true), 2500);
+  }, []);
 
   return (
     <Scrollbars
       onMouseMove={handleMouseActivity}
       onWheel={handleMouseActivity}
       className={classes.scrollbar}
-      autoHide={isMouseMoving}
+      autoHide={hideScrollbar}
       autoHideTimeout={0}
       autoHideDuration={300}
       renderThumbVertical={(props) => (
