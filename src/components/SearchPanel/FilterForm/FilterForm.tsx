0321e0890f5d7ae0574e9a2f87087365c324844d
airfortech <airm@xxx.yy>
2022-08-05 00:25:02 +0200

    added reset form values functionality

--- src/components/SearchPanel/FilterForm/FilterForm.tsx
+++ src/components/SearchPanel/FilterForm/FilterForm.tsx
@@ -42,111 +42,117 @@ const printValues = (values: InitialValues) => {
 export const FilterForm = ({ closeModal }: Props) => {
   return (
     <div className={classes.FilterForm}>
-      <div className={classes.row}>
-        <h2>Filtrowanie</h2>
-        <PrimaryButton size="normal" color="quaternary">
-          Wyczyść wszystkie
-        </PrimaryButton>
-      </div>
       <Formik
         initialValues={initialValues}
         onSubmit={(values) => alert(printValues(values))}
       >
-        <Form className={classes.form}>
-          <FormGroup title="Ocena przejścia kursu">
-            {degrees.map(({ value, name }) => (
-              <CheckItem
-                type="radio"
-                groupName="courseCompletion"
-                value={value}
-                name={name}
-                icon="star"
-                key={value}
-              />
-            ))}
-          </FormGroup>
-          <FormGroup title="Ocena aktywności i zaangażowania na kursie">
-            {degrees.map(({ value, name }) => (
-              <CheckItem
-                type="radio"
-                groupName="courseEngagment"
-                value={value}
-                name={name}
-                icon="star"
-                key={value}
-              />
-            ))}
-          </FormGroup>
-          <FormGroup title="Ocena kodu w projekcie własnym">
-            {degrees.map(({ value, name }) => (
-              <CheckItem
-                type="radio"
-                groupName="projectDegree"
-                value={value}
-                name={name}
-                icon="star"
-                key={value}
-              />
-            ))}
-          </FormGroup>
-          <FormGroup title="Ocena pracy w zespole w Scrum">
-            {degrees.map(({ value, name }) => (
-              <CheckItem
-                type="radio"
-                groupName="teamProjectDegree"
-                value={value}
-                name={name}
-                icon="star"
-                key={value}
-              />
-            ))}
-          </FormGroup>
-          <FormGroup title="Preferowane miejsce pracy">
-            {expectedTypeWork.map(({ value, name }) => (
-              <CheckItem
-                type="checkbox"
-                groupName="expectedTypeWork"
-                value={value}
-                name={name}
-                key={value}
-              />
-            ))}
-          </FormGroup>
-          <FormGroup title="Oczekiwany typ kontraktu">
-            {expectedContractType.map(({ value, name }) => (
-              <CheckItem
-                type="checkbox"
-                groupName="expectedContractType"
-                value={value}
-                name={name}
-                key={value}
-              />
-            ))}
-          </FormGroup>
-          <FormGroup title="Zgoda na odbycie bezpłatnych praktyk/stażu na początek">
-            {canTakeApprenticeship.map(({ value, name }) => (
-              <CheckItem
-                type="radio"
-                groupName="canTakeApprenticeship"
-                value={value}
-                name={name}
-                key={value}
-              />
-            ))}
-          </FormGroup>
-          <div className={classes.bottomButtons}>
-            <PrimaryButton
-              size="normal"
-              color="quaternary"
-              onClick={closeModal}
-            >
-              Anuluj
-            </PrimaryButton>
-            <PrimaryButton type="submit" onClick={closeModal}>
-              Pokaż wyniki
-            </PrimaryButton>
-          </div>
-        </Form>
+        {({ resetForm }) => (
+          <Form className={classes.form}>
+            <div className={classes.row}>
+              <h2>Filtrowanie</h2>
+              <PrimaryButton
+                size="normal"
+                color="quaternary"
+                onClick={() => resetForm()}
+              >
+                Wyczyść wszystkie
+              </PrimaryButton>
+            </div>
+            <FormGroup title="Ocena przejścia kursu">
+              {degrees.map(({ value, name }) => (
+                <CheckItem
+                  type="radio"
+                  groupName="courseCompletion"
+                  value={value}
+                  name={name}
+                  icon="star"
+                  key={value}
+                />
+              ))}
+            </FormGroup>
+            <FormGroup title="Ocena aktywności i zaangażowania na kursie">
+              {degrees.map(({ value, name }) => (
+                <CheckItem
+                  type="radio"
+                  groupName="courseEngagment"
+                  value={value}
+                  name={name}
+                  icon="star"
+                  key={value}
+                />
+              ))}
+            </FormGroup>
+            <FormGroup title="Ocena kodu w projekcie własnym">
+              {degrees.map(({ value, name }) => (
+                <CheckItem
+                  type="radio"
+                  groupName="projectDegree"
+                  value={value}
+                  name={name}
+                  icon="star"
+                  key={value}
+                />
+              ))}
+            </FormGroup>
+            <FormGroup title="Ocena pracy w zespole w Scrum">
+              {degrees.map(({ value, name }) => (
+                <CheckItem
+                  type="radio"
+                  groupName="teamProjectDegree"
+                  value={value}
+                  name={name}
+                  icon="star"
+                  key={value}
+                />
+              ))}
+            </FormGroup>
+            <FormGroup title="Preferowane miejsce pracy">
+              {expectedTypeWork.map(({ value, name }) => (
+                <CheckItem
+                  type="checkbox"
+                  groupName="expectedTypeWork"
+                  value={value}
+                  name={name}
+                  key={value}
+                />
+              ))}
+            </FormGroup>
+            <FormGroup title="Oczekiwany typ kontraktu">
+              {expectedContractType.map(({ value, name }) => (
+                <CheckItem
+                  type="checkbox"
+                  groupName="expectedContractType"
+                  value={value}
+                  name={name}
+                  key={value}
+                />
+              ))}
+            </FormGroup>
+            <FormGroup title="Zgoda na odbycie bezpłatnych praktyk/stażu na początek">
+              {canTakeApprenticeship.map(({ value, name }) => (
+                <CheckItem
+                  type="radio"
+                  groupName="canTakeApprenticeship"
+                  value={value}
+                  name={name}
+                  key={value}
+                />
+              ))}
+            </FormGroup>
+            <div className={classes.bottomButtons}>
+              <PrimaryButton
+                size="normal"
+                color="quaternary"
+                onClick={closeModal}
+              >
+                Anuluj
+              </PrimaryButton>
+              <PrimaryButton type="submit" onClick={closeModal}>
+                Pokaż wyniki
+              </PrimaryButton>
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
