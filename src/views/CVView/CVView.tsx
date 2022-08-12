648ee344d2801c57f3ca708e497640e6130be088
airfortech <airm@xxx.yy>
2022-08-12 14:15:19 +0200

    added back button to CVVieW

--- src/views/CVView/CVView.tsx
+++ src/views/CVView/CVView.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { PersonalDetails } from "../../components/PersonalDetails/PersonalDetails";
 import { TechDetails } from "../../components/TechDetails/TechDetails";
 import classes from "./CVView.module.css";
 
 export const CVView = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={classes.CVView}>
-      <PersonalDetails />
-      <TechDetails />
+      <button className={classes.back} onClick={() => navigate(-1)}>
+        <i className="bx bxs-chevron-left"></i>
+        Wróć
+      </button>
+      <div className={classes.row}>
+        <PersonalDetails />
+        <TechDetails />
+      </div>
     </div>
   );
 };
