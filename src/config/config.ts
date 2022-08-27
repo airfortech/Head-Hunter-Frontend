e4a3106a73efdfd8e52407f50c8ef7f011823775
airfortech <airm@xxx.yy>
2022-08-28 01:18:39 +0200

    added env files

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@ export const config = {
   resultsPerPageOptions: [
     5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ],
-  defaultResultsPerPageOption: 5,
-  apiUrl: "http://localhost:3001/api/v1/",
+  defaultResultsPerPageOption:
+    Number(process.env.REACT_APP_defaultResultsPerPageOption) || 5,
+  apiUrl: process.env.REACT_APP_apiUrl || "http://localhost:3001/api/v1/",
 };
